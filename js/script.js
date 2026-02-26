1cdc21761a40be42c5a092472e3de519ac8b0fbd
Yehochanan <14306925+yehochanan@users.noreply.github.com>
2026-02-26 15:35:58 -0600

    randomly generate rock paper or scissors
    generate computer choice based on a random number between 1 and 3
    
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   js/script.js
    	new file:   pseudoCode/computerChoice.dscode
    	new file:   pseudoCode/diagrams/.~lock.rps_diagrams.ods#
    	modified:   pseudoCode/diagrams/rps_diagrams.ods

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-console.log("Hello World!");
+function getComputerChoice(){
+
+    // randomNumber = Random number between 1 and 3
+    randomNumber = Math.floor(Math.random()*3) + 1;
+    // console.log(randomNumber);
+
+    // print rock paper or scissors
+    if (randomNumber === 1) {
+        console.log("rock");
+    } else if (randomNumber === 2){
+        console.log("paper");
+    } else {
+        console.log("scissors");
+    }
+}
+
+getComputerChoice();
