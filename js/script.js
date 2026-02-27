2ca07f959cdd3a1617791b43f81c1f6a3ada68f6
Yehochanan <14306925+yehochanan@users.noreply.github.com>
2026-02-26 21:40:26 -0600

    add gameplay through func playRound
    Added gameplay through the function playRound after
    designing in pseudoCode. design was altered after
    implementation as logic proved not to work.
    
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   js/script.js
    	new file:   pseudoCode/diagrams/.~lock.rps_diagrams.ods#
    	modified:   pseudoCode/diagrams/rps_diagrams.ods
    	new file:   pseudoCode/playRound.dscode

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 let humanScore = 0;
 let computerScore = 0;
 
+
+// Get computer choice
+
 function getComputerChoice(){
 
     // randomNumber = Random number between 1 and 3
@@ -9,13 +12,17 @@ function getComputerChoice(){
 
     // print rock paper or scissors
     if (randomNumber === 1) {
-        console.log("rock");
+        // console.log("rock");
+        return "rock";
     } else if (randomNumber === 2){
-        console.log("paper");
+        // console.log("paper");
+        return "paper";
     } else {
-        console.log("scissors");
+        // console.log("scissors");
+        return "scissors";
     }
 }
+// get human choice
 
 function getHumanChoice(){
     // PROMPT for choice
@@ -24,8 +31,80 @@ function getHumanChoice(){
     // parse choice to be all lowercase
     let choice = player_choice.toLowerCase();
     // return parsed choice
-    console.log(choice);
+    // console.log(choice);
+    return choice;
 }
 
-getComputerChoice();
-getHumanChoice();
+// win - loose text
+function winText(){
+    console.log("Congrats! You won");
+}
+
+function looseText(){
+    console.log("Better luck next time");
+}
+
+function tieText(){
+    console.log("A draw, too bad")
+}
+
+// single round of rps
+
+function playRound(HumanChoice, ComputerChoice){
+    // Testing function
+    console.log("Player chose: " + HumanChoice);
+    console.log("Computer chose: " + ComputerChoice);
+
+    // variables
+    let computerWins = false;
+    let playerWins = false;
+
+    // Game mechanics for rps
+    if (HumanChoice === "rock" && ComputerChoice === "paper") {
+        looseText();
+        // return computerWins = true;
+        return computerScore++;
+    } else if (HumanChoice === "rock" && ComputerChoice === "scissors") {
+        winText();
+        // return playerWins = true;
+        return humanScore++;
+    } else if (HumanChoice === "paper" && ComputerChoice === "rock") {
+        winText();
+        // return playerWins = true;
+        return humanScore++;
+    } else if (HumanChoice === "paper" && ComputerChoice === "scissors") {
+        looseText();
+        // return computerWins = true;
+        return computerScore++;
+    } else if (HumanChoice === "scissors" && ComputerChoice === "rock") {
+        looseText();
+        // return computerWins = true;
+        computerScore++;
+    } else if (HumanChoice === "scissors" && ComputerChoice === "paper") {
+        winText();
+        // return playerWins = true;
+        return humanScore++;
+    } else {
+        tieText();
+    }
+    
+    // if (computerWins === true) {
+    //     computerScore++;
+    // } else if (playerWins === true) {
+    //     humanScore++;
+    // } else {
+    //     // do nothing
+    // }
+} 
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+
+
+// getComputerChoice();
+// getHumanChoice();
+playRound(humanSelection,computerSelection);
+
+console.log("The Computers current score is: " + computerScore);
+console.log("The players current score is: " + humanScore);
